610efc1394da0562b64a88cb4dd2281ba39f6269
Louis Masurel <masurellouis@gmail.com>
2018-04-23 18:08:38 +0200

    quotations-list

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/quotations', (req, res, next) => {
+  res.render('quotations');
+});
+
+router.get('/invoices', (req, res, next) => {
+  res.render('invoices');
+});
+
+router.get('/delivreryforms', (req, res, next) => {
+  res.render('delivreryforms');
+});
+
+router.get('/customers', (req, res, next) => {
+  res.render('customers');
+});
+
+router.get('/products', (req, res, next) => {
+  res.render('products');
+});
+
+router.get('/customers', (req, res, next) => {
+  res.render('customers');
+});
+
+
 module.exports = router;
