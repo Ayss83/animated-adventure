9e548f60153b2b19e4ead8484974e8bf3fb4b5be
Louis Masurel <masurellouis@gmail.com>
2018-04-23 19:40:29 +0200

    index modifié

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,5 @@ router.get('/', (req, res, next) => {
   res.render('dashboard', {navColor: "is-primary"});
 });
 
-router.get('/quotations', (req, res, next) => {
-  res.render('quotations');
-});
-
-router.get('/invoices', (req, res, next) => {
-  res.render('invoices');
-});
-
-router.get('/delivreryforms', (req, res, next) => {
-  res.render('delivreryforms');
-});
-
-router.get('/customers', (req, res, next) => {
-  res.render('customers');
-});
-
-router.get('/products', (req, res, next) => {
-  res.render('products');
-});
-
-router.get('/customers', (req, res, next) => {
-  res.render('customers');
-});
-
 
 module.exports = router;
